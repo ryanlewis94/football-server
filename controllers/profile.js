e1efa61c4fe9580ceeb4f0a489747578037c4ace
Ryan Lewis <1994ryanlewis1994@gmail.com>
2019-04-13 21:39:17 +0100

    initial commit

---
+++ controllers/profile.js
@@ -0,0 +1,19 @@
+const handleProfile = (req, res, db) => {
+	const { id } = req.params;
+	db
+		.select('*')
+		.from('users')
+		.where({ id })
+		.then((user) => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('Error 400');
+			}
+		})
+		.catch((err) => res.status(400).json('Not found'));
+};
+
+module.exports = {
+	handleProfile
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const comment = require('./controllers/comment');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'postgres',
+		password: 'pass',
+		database: 'football'
+	}
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+});
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => {
+	register.handleRegister(req, res, db, bcrypt);
+});
+app.get('/profile/:id', (req, res) => {
+	profile.handleProfile(req, res, db);
+});
+app.put('/comment', (req, res) => {
+	comment.handleComment(req, res, db);
+});
+
+app.listen(3000, () => {
+	console.log('app is running on 3000');
+});
